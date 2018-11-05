4c368d9e98ca3ae5bb00afe95e28f124937b6735
‘Brittany <brittany.stubbs1827@gmail.com>
2018-11-05 15:44:05 -0500

    still messing with date

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/weather', (request, response) => {
     const url = 'https://api.darksky.net/forecast/926bad7492c47794153864832ffd41aa/37.8267,-122.4233'
     superagent.get(url)
       .then(res => response.send({
-        time: res.body.daily.data[0].time.toString(),
+        time: ((res.body.daily.data[0].time)*1000).toString(),
         forecast: res.body.daily.data[0].summary
       }))
       .catch(err => response.send('<img src="http://http.cat/404"/>'))
